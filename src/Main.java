8964efc7ee8926f3aa68cf1822dd714d192dbb79
Emilie Anthony <emilieanthony@live.se>
2020-11-19 23:45:39 +0100

    Create ProductOwnerView class, create method for creating and viewing product backlog

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,38 @@
+import Models.BacklogModel;
+import Models.UserStoryModel;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main (String [] args) {
-        System.out.println("New branch test");
 
+        ProductOwnerView menuProductOwner = new ProductOwnerView();
+        // Ska in till controller
+        BacklogModel backlogModel = menuProductOwner.createBacklog();
+        //Ta bort - enbart för test
+        UserStoryModel userStoryModel = new UserStoryModel("UserStory 1", "2", "5", 3, 3, "As a user, I want to be happy so that..", "1.1 Be good");
+        //Ta bort - enbart för test
+        ArrayList <UserStoryModel> userStories = new ArrayList<>();
+        //Ta bort - enbart för test
+        userStories.add(userStoryModel);
+        //Ta bort - enbart för test
+        backlogModel.setAllUserStories(userStories);
+        //Ta bort - enbart för test
+        menuProductOwner.viewBacklog (backlogModel);
+
+        // Ska in till controller
+        int option = menuProductOwner.menuProductOwner();
+        switch (option) { //put switch in controller and call methods? Controller: "Accepts input and converts it to commands for the model or view"
+            case 1:
+                menuProductOwner.createBacklog();
+
+            case 2:
+                menuProductOwner.viewBacklog(backlogModel);
+
+            case 3:
+                // go back to main menu
+        }
 
     }
 }
--- src/ProductOwnerView.java
+++ src/ProductOwnerView.java
@@ -1,18 +1,21 @@
+import Models.BacklogModel;
+import Models.UserStoryModel;
+
 import java.util.Scanner;
 
-public class MenuProductOwner {
+public class ProductOwnerView {
     Scanner input = new Scanner(System.in);
 
-    public void menuProductOwner() {
+    public int menuProductOwner() {
 
         System.out.println("Welcome product owner!\n" +
                 "Please enter an option below\n" +
                 "1. Create a new product backlog\n" +
                 "2. View product backlog\n" +
                 "3. Go back to main menu\n");
-        int option = input.nextInt();
 
-        switch (option) { //put switch in controller and call methods?
+        int option = input.nextInt();
+        switch (option) { //put switch in controller and call methods? Controller: "Accepts input and converts it to commands for the model or view"
             case 1:
                 createBacklog();
 
@@ -22,10 +25,11 @@ public class MenuProductOwner {
             case 3:
                 // go back to main menu
         }
+        return input.nextInt(); // För switchen i controllern
 
     }
 
-    public Backlog createBacklog() {
+    public BacklogModel createBacklog() {
         System.out.println("Please enter product backlog name:");
         String backlogName = input.nextLine();
         System.out.println("Please enter start date:");
@@ -33,12 +37,11 @@ public class MenuProductOwner {
         System.out.println("Please enter end date:");
         String endDate = input.nextLine();
 
-        return new Backlog(backlogName + startDate + endDate);
+        return new BacklogModel(backlogName, startDate, endDate);
     }
 
-    //public void viewBacklog(){
-    //loop and print back log
-    //}
+    public void viewBacklog(BacklogModel backlogModel){
+        System.out.println(backlogModel.toString());
 
-
+    }
 }
